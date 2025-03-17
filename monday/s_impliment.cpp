fe31755fa43c8bbc0f1d78f84600b6f23f60d663
Urvesh0069 <urveshghodadara82@gmail.com>
2025-03-17 10:26:20 +0530

    monday

---
+++ monday/s_impliment.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class Stack {
+private:
+    int* arr;
+    int topIndex;
+    int capacity;
+
+public:
+    Stack(int size) {
+        capacity = size;
+        arr = new int[capacity];
+        topIndex = -1;
+    }
+
+    int getTop() {
+        if(topIndex == -1) {
+            cout << "Stack underflow\n";
+            return -1;
+        }
+
+        return arr[topIndex];
+    }
+
+    void push(int value) {
+        if (topIndex == capacity - 1) {
+            cout << "Stack overflow\n";
+            return;
+        }
+        arr[++topIndex] = value;
+        cout << value << " pushed into stack\n";
+    }
+
+    int pop() {
+        if (topIndex == -1) {
+            cout << "Stack underflow\n";
+            return -1;
+        }
+        int poppedValue = arr[topIndex--];
+        return poppedValue;
+    }
+
+    bool isEmpty() {
+        return topIndex == -1;
+    }
+
+};
+
+int main() {
+    Stack stack(5);
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    stack.pop();
+
+    cout << "Top element: " << stack.getTop() << endl;
+
+    return 0;
+}
